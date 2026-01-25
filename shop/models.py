f7fa25f77f64359f7c844ad30ee0765f778b0983
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-01-25 21:39:54 +0300

    shop/models.py adjustment and add css, img

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -15,8 +16,15 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+
     def __str__(self):
         return self.name
+    
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+    
 
 
 class Product(models.Model):
@@ -46,3 +54,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
