8b6b4c375bfc8d054f81e439457002f17fc7744b
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-01-27 23:00:31 +0300

    Add new app - cart and cart/cart.py

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'midia/'
 MEDIA_ROOT = BASE_DIR / 'media'
+CART_SESSION_ID = 'cart'
